664fdcb1a187dfcbf9b5379d33006baedb293448
Maxmiliano <74252542+Maxmiliano-Maia@users.noreply.github.com>
2025-01-26 16:17:16 -0300

    Tamplates

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,5 +1,10 @@
 @extends('site.layout')
 @section('title', 'Essa é a página HOME')
 @section('conteudo')
-<h1> Essa é a nossa home </h1>
+{{-- Isso é um comentario--}}
+
+{{--isset($nome) ? 'existe' : 'não existe'--}}
+
+{{$teste ?? 'padrçao'}}
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,3 @@ use App\Http\Controllers\ProdutoController;
 Route::resource('produtos',ProdutoController::class);
 
 
-
